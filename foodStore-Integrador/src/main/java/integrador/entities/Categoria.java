bc0dfa2880d6c94312c5978f070ee2a0a8edb524
Gonza-12-Sanchez <goncimatisanchez@gmail.com>
2026-06-09 20:47:33 -0300

    Cambios generales

--- foodStore-Integrador/src/main/java/integrador/entities/Categoria.java
+++ foodStore-Integrador/src/main/java/integrador/entities/Categoria.java
@@ -1,16 +1,18 @@
 package integrador.entities;
 
 public class Categoria extends Base{
+    //Atributos
     private String nombre;
     private String descripcion;
 
+    //Constructor
     public Categoria(String nombre,String descripcion){
         super();
         this.nombre = nombre;
         this.descripcion = descripcion;
     }
 
+    //Getters
     public String getNombre() {return nombre;}
-
     public String getDescripcion() {return descripcion;}
 }
